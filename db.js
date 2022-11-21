2aae151a59dd6d973894ec6348db644a26656462
Parham Abr <abhar@globalworkandtravel.com>
2022-11-21 20:05:22 +0330

    all files

---
+++ db.js
@@ -0,0 +1,184 @@
+const { Client } = require('pg')
+
+let client = null;
+
+const GAME_STATUS = {
+    NOT_STARTED: 0,
+    STARTED: 1,
+    FINISHED: 2,
+}
+
+async function connect() {
+    client = new Client({
+        user: 'kblurtgdeqwwnk',
+        host: 'ec2-34-247-118-233.eu-west-1.compute.amazonaws.com',
+        database: 'deu33qntuk6utj',
+        password: 'e8bbd36eba3c6a952892879a42a4e6ddc09f4e5efa6548702235a931c9b7a5a8',
+        port: 5432,
+        ssl: { rejectUnauthorized: false }
+    });
+    await client.connect();
+}
+
+
+async function calculateUserScores(matchId, team1Goals, team2Goals, winner, isGroup) {
+    const query = `SELECT * FROM user_match LEFT JOIN users
+            ON users.id = user_match.user_id
+            WHERE user_match.match_id = ${matchId}`;
+    const result = await client.query(query);
+
+    const promises = [];
+    
+    for (let i = 0; i < result.rows.length; i++) {
+        let userMatch = result.rows[i];
+        let predictionTeam1Goals = userMatch.team1_goals;
+        let predictionTeam2Goals = userMatch.team2_goals;
+        let predictionWinner = userMatch.winner;
+
+        let userScore = 0;
+        if (predictionTeam1Goals == team1Goals && predictionTeam2Goals == team2Goals) {
+            userScore = 15;
+            
+        } else if ((predictionTeam1Goals - predictionTeam2Goals) == (team1Goals - team2Goals)) {
+            userScore = 10;
+        } else if (isGroup && predictionWinner == winner) {
+            userScore = 5;
+        } else if (!isGroup) {
+            if ((predictionTeam1Goals > predictionTeam2Goals && team1Goals > team2Goals) || 
+                (predictionTeam1Goals < predictionTeam2Goals && team1Goals < team2Goals)) {
+                 userScore = 5;
+            }
+        }
+
+        if (!isGroup && predictionWinner == winner) {
+            userScore += 10;
+        }
+
+        promises.push(client.query(`UPDATE users SET score = score+${userScore} WHERE id = ${userMatch.user_id}`));
+    }
+
+    await Promise.all(promises);
+}
+
+module.exports = {
+    newUser: async (username) => {
+        await connect();
+        const users = await client.query(`
+            select * from users where username = '${username}'
+        `);
+
+        if (users.rows.length === 0) {
+            const query = 'INSERT INTO users(username, score) VALUES($1, $2)';
+            const values = [username, 0];
+            await client.query(query, values);
+        }
+        // await end();
+        
+    },
+    
+    newMatch: async (team1, team2, isGroup) => {
+        const query = `INSERT INTO matches(team1, team2, is_group, status) VALUES($1, $2, $3, $4)`;
+        const values = [team1, team2, isGroup, GAME_STATUS.NOT_STARTED];
+        await client.query(query, values);
+    },
+
+    getNotStartedMatches: async () => {
+        const query = `select * from matches where status = ${GAME_STATUS.NOT_STARTED}`;
+        const result = await client.query(query);
+        return result.rows;
+    },
+
+    getStartedMatches: async () => {
+        const query = `select * from matches where status = ${GAME_STATUS.STARTED}`;
+        const result = await client.query(query);
+        return result.rows;
+    },
+
+    setGameAsStarted: async(matchId) => {
+        const query = `UPDATE matches SET status = ${GAME_STATUS.STARTED} WHERE id = ${matchId}`;
+        await client.query(query);
+    },
+
+    getMatchById: async(matchId) => {
+        const query = `SELECT * FROM matches WHERE id = ${matchId}`;
+        const result = await client.query(query);
+        return result.rows[0];
+    },
+
+    setGameAsFinished: async(matchId, team1Goals, team2Goals, winner, isGroup) => {
+        const query = `UPDATE matches SET status = ${GAME_STATUS.FINISHED},
+                        team1_result = ${team1Goals},
+                        team2_result = ${team2Goals},
+                        winner = '${winner}'
+                        WHERE id = ${matchId}`;
+        await client.query(query);
+
+        await calculateUserScores(matchId, team1Goals, team2Goals, winner, isGroup);
+    },
+
+    bet: async(matchId, team1Goals, team2Goals, winner, username) => {
+        let query = `SELECT * FROM users WHERE username = '${username}'`;
+        let result = await client.query(query);
+        const user = result.rows[0];
+
+        query = `SELECT * FROM user_match WHERE user_id = ${user.id} AND match_id = ${matchId}`;
+        result = await client.query(query);
+        const won = winner || 'team10';
+        if (result.rows.length) {
+            //ALREADY BET
+            query = `UPDATE user_match SET 
+                    team1_goals = ${team1Goals},
+                    team2_goals = ${team2Goals},
+                    winner = '${won}'
+                    WHERE user_id = ${user.id} AND match_id = ${matchId}`;
+            await client.query(query);
+
+        } else {
+            //NOT BET
+            query = `INSERT INTO user_match(team1_goals, team2_goals, user_id, winner, match_id) VALUES($1, $2, $3, $4, $5)`;
+            const values = [team1Goals, team2Goals, user.id, winner, matchId];
+            await client.query(query, values);
+        }
+    },
+
+    getUsersRank: async() => {
+        let query = `SELECT * FROM users`;
+        let result = await client.query(query);
+        const users = result.rows;
+        return users;
+    },
+
+    getUsersPrediction: async(matchId) => {
+        const fields = [
+            'um.team1_goals',
+            'um.team2_goals',
+            'u.username',
+            'um.winner AS prediction_winner',
+            'm.team1',
+            'm.team2',
+            'm.is_group',
+        ]
+        const query = `select ${fields.join(', ')} from user_match um 
+                        left join users u on u.id = um.user_id 
+                        left join matches m on m.id = um.match_id 
+                        where m.id = ${matchId}`;
+        const result = await client.query(query);
+
+        const msg = [];
+
+        for (let i = 0; i < result.rows.length; i++) {
+            let item = result.rows[i];
+            let str = `${item.username}: ${item.team1} ${item.team1_goals} - ${item.team2_goals} ${item.team2}`;
+            if (!item.is_group && item.team1_goals == item.team2_goals) {
+                str += ` ,${item.prediction_winner} wins.`;
+            }
+            msg.push(str);
+        }
+
+        return msg;
+    },
+
+    
+
+
+}
---
+++ index.js
@@ -0,0 +1,419 @@
+const { Telegraf } = require('telegraf');
+const express = require('express');
+const DB = require('./db');
+const expressApp = express();
+
+const TOKEN = '1708052796:AAFar1GGlUl4PA7sF5swwIPxVR_-j1SB56I';
+const URL = 'https://euro2021bet.herokuapp.com';
+// const URL = 'https://f9ebc4f00be7.ngrok.io';
+const bot = new Telegraf(TOKEN);
+
+const PORT = process.env.PORT || 433;
+
+bot.telegram.setWebhook(`${URL}/bot${TOKEN}` , {
+    source: './certs/crt.pem'
+});
+expressApp.use(bot.webhookCallback(`/bot${TOKEN}`));
+
+expressApp.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+expressApp.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
+bot.launch();
+
+// startPolling();
+
+// async function startPolling() {
+//     try {
+//         await bot.telegram.deleteWebhook();
+//         await bot.startPolling();
+//     } catch(e) {
+//         console.log('GOT IT!');
+//     }
+// }
+
+const TASKS = {
+    CREATE_NEW_GAME: '0',
+    SET_GAME_AS_BEGIN: '1',
+    SET_GAME_AS_FINISHED: '2',
+    BETTING: '3',
+}
+
+const data = {
+
+};
+
+bot.command('start', async ctx => {
+    console.log('--ON START');
+    const username = ctx.from.username;
+    
+    try {
+        await DB.newUser(username);
+
+        if (!data.hasOwnProperty(username)) {
+            data[username] = {
+                task: null,
+            };
+        }
+
+        bot.telegram.sendMessage(ctx.chat.id, `Hello ${ctx.from.username}, Click /menu`, {});
+    } catch(e) {
+        console.log('eror: ', e);
+        bot.telegram.sendMessage(ctx.chat.id, `Error initializing user, Click /start again.`, {});
+    }
+});
+
+bot.command('cancel', async ctx => {
+    console.log('--ON CANCEL');
+    const username = ctx.from.username;
+
+    if (data[username]) {
+        data[username] = {};
+    }
+
+    bot.telegram.sendMessage(ctx.chat.id, 'Cancelled!', mainMenu);
+});
+
+bot.command('menu', (ctx) => {
+    const username = ctx.from.username;
+    const id = ctx.chat.id;
+    if (!validateUserName(username)) {
+        return sendStartAgainMessage(id);
+    }
+    
+    bot.telegram.sendMessage(id, 'Menu:', mainMenu);
+});
+
+bot.command('quit', (ctx) => {
+    // Explicit usage
+    ctx.telegram.leaveChat(ctx.message.chat.id)
+  
+    // Using context shortcut
+    ctx.leaveChat()
+});
+
+function creatingNewGame(id, username) {
+    console.log('--Create New Match');
+    if (!validateUserName(username)) {
+        return sendStartAgainMessage(id);
+    }
+
+    data[username].task = TASKS.CREATE_NEW_GAME;
+
+    bot.telegram.sendMessage(id, 'Enter First Team Name: (/cancel)', {});
+}
+
+async function gameStarted(id, username) {
+    console.log('--Match Started');
+    if (!validateUserName(username)) {
+        return sendStartAgainMessage(id);
+    }
+
+    try {
+        const matches = await DB.getNotStartedMatches();
+        if (matches.length) {
+            data[username].task = TASKS.SET_GAME_AS_BEGIN;
+            bot.telegram.sendMessage(id, 'Which Match? (/cancel)', {
+                "reply_markup": {
+                    keyboard: matches.map(match => [{text: `${match.id}_${match.team1}-${match.team2}`}])
+                }
+            });    
+        } else {
+            bot.telegram.sendMessage(id, 'No Match Available', mainMenu);
+        }
+    } catch(e) {
+        bot.telegram.sendMessage(id, 'Error during "match started", Plz try again."', mainMenu);
+    }
+
+    
+}
+
+async function gameFinished(id, username) {
+    console.log('--Match Finished');
+    if (!validateUserName(username)) {
+        return sendStartAgainMessage(id);
+    }
+
+    try {
+        const matches = await DB.getStartedMatches();
+        if (matches.length) {
+            data[username].task = TASKS.SET_GAME_AS_FINISHED;
+            bot.telegram.sendMessage(id, 'Which Match? (/cancel)', {
+                "reply_markup": {
+                    keyboard: matches.map(match => [{text: `${match.id}_${match.team1}-${match.team2}`}])
+                }
+            });    
+        } else {
+            bot.telegram.sendMessage(id, 'No Match Available', mainMenu);    
+        }
+    } catch(e) {
+        bot.telegram.sendMessage(id, 'Error during "match finished", Plz try again."', mainMenu);
+    }
+
+    
+
+}
+
+async function letsBet(id, username) {
+    console.log('--Start Betting');
+    if (!validateUserName(username)) {
+        return sendStartAgainMessage(id);
+    }
+
+    try {
+        const matches = await DB.getNotStartedMatches();
+        if (matches.length) {
+            data[username].task = TASKS.BETTING;
+            bot.telegram.sendMessage(id, 'Which Match? (/cancel)', {
+                "reply_markup": {
+                    keyboard: matches.map(match => [{text: `${match.id}_${match.team1}-${match.team2}`}])
+                }
+            });    
+        } else {
+            bot.telegram.sendMessage(id, 'No Match Available', mainMenu);
+        }
+    } catch(e) {
+        bot.telegram.sendMessage(id, 'Error during "lets bet", Plz try again."', mainMenu);
+    }
+}
+
+async function showUsersRank(id) {
+    try {
+        const users = await DB.getUsersRank();
+        const usersRank = users.map(user => `${user.username}: ${user.score}`);
+        bot.telegram.sendMessage(id, usersRank.join('\n'), mainMenu);
+    } catch(e) {
+        bot.telegram.sendMessage(id, 'Error during "Show Rank", Plz try again."', mainMenu);
+    }
+}
+
+
+bot.on('text', async (ctx) => {
+    const username = ctx.from.username;
+    const msg = ctx.message.text;
+    const id = ctx.chat.id;
+
+    if (!validateUserName(username)) {
+        return sendStartAgainMessage(id);
+    }
+
+    switch (msg) {
+        case 'New Match':
+            return creatingNewGame(id, username);
+        case 'Match Started':
+            return gameStarted(id, username);
+        case 'Match Finished':
+            return gameFinished(id, username);
+        case "Let's Bet!":
+            return letsBet(id, username);
+        case "Users Rank":
+            return showUsersRank(id);
+    }
+
+    try {
+        if (data[username].task == TASKS.CREATE_NEW_GAME) {
+            if (data[username].team2) {
+                //GETTING IF IS GROUP OR NOT
+                data[username].is_group = msg === 'Yes' ? true : false;
+                // console.log(data);
+                await DB.newMatch(data[username].team1, data[username].team2, data[username].is_group);
+                data[username] = {};
+                bot.telegram.sendMessage(id, 'Done, you can use /menu again.', mainMenu);
+            
+            } else if (data[username].team1) {
+                //GETTING Group Stage
+                data[username].team2 = msg;
+                bot.telegram.sendMessage(id, 'Group Stage? (/cancel)', {
+                    "reply_markup": {
+                        keyboard: [
+                            [{text: "Yes"}],
+                            [{text: "No"}],
+                        ]
+                    }
+                });
+                data[username].is_group = true;
+    
+            } else {
+                //GETTING TEAM 2
+                data[username].team1 = msg;
+                bot.telegram.sendMessage(id, 'Enter Second Team Name: (/cancel)', {});
+    
+            }
+        }
+    
+    
+        if (data[username].task == TASKS.SET_GAME_AS_BEGIN) {
+            const matchId = msg.split('_')[0];
+            const teams = msg.split('_')[1];
+            const team1 =  teams.split('-')[0];
+            const team2 =  teams.split('-')[1];
+            await DB.setGameAsStarted(matchId);
+            const result = await DB.getUsersPrediction(matchId);
+            data[username] = {};
+            bot.telegram.sendMessage(id, `Match: ${team1}-${team2} just started.\nPredictions:\n---\n${result.join('\n---\n')}`, mainMenu);
+        }
+    
+        
+        if (data[username].task == TASKS.SET_GAME_AS_FINISHED) {
+            if (!data[username].match) {
+                const matchId = msg.split('_')[0];
+                const match = await DB.getMatchById(matchId);
+                data[username].match = match;
+            }
+            
+            const matchId = data[username].match.id;
+            const team1 = data[username].match.team1;
+            const team2 = data[username].match.team2;
+    
+            if (data[username].team2_goals != undefined) {
+                const won = msg;
+                let winner = '';  
+                if (won == team1) {
+                    winner = 'team1';
+                } else {
+                    winner = 'team2';
+                }
+                const team1Goals = data[username].team1_goals;
+                const team2Goals = data[username].team2_goals;
+                await DB.setGameAsFinished(matchId, team1Goals, team2Goals, winner, data[username].match.is_group);
+                const result = await DB.getUsersPrediction(matchId);
+                data[username] = {};
+                bot.telegram.sendMessage(id, `Match finished, ${team1} ${team1Goals} - ${team2Goals} ${team2} , ${won} won.\nPredictions:\n---\n${result.join('\n---\n')}`, mainMenu);
+            } else if (data[username].team1_goals != undefined) {
+                data[username].team2_goals = Number(msg);
+                
+                if (data[username].team1_goals == data[username].team2_goals && !data[username].match.is_group) {
+                    bot.telegram.sendMessage(id, `Which team won? (/cancel)`, {
+                        "reply_markup": {
+                            keyboard: [
+                                [{text: team1}],
+                                [{text: team2}],
+                            ]
+                        }
+                    });
+                } else {
+                    let winner = null;
+                    if (data[username].team1_goals > data[username].team2_goals) {
+                        winner = 'team1';
+                    } else if (data[username].team1_goals < data[username].team2_goals) {
+                        winner = 'team2';
+                    }
+                    const team1Goals = data[username].team1_goals;
+                    const team2Goals = data[username].team2_goals;
+                    await DB.setGameAsFinished(matchId, team1Goals, team2Goals, winner, data[username].match.is_group);
+                    const result = await DB.getUsersPrediction(matchId);
+                    data[username] = {};
+                    bot.telegram.sendMessage(id, `Match finished, ${team1} ${team1Goals} - ${team2Goals} ${team2}.\nPredictions:\n---\n${result.join('\n----\n')}`, mainMenu);
+                }
+            } else if (data[username].getting_goals) {
+                data[username].team1_goals = Number(msg);
+                bot.telegram.sendMessage(id, `How many goals ${team2} scored? (/cancel)`, {});
+            } else {
+                //GETTING TEAM 1 GOALS
+                data[username].getting_goals = true;
+                bot.telegram.sendMessage(id, `How many goals ${team1} scored? (/cancel)`, {});
+            }
+    
+        }
+    
+    
+        if (data[username].task == TASKS.BETTING) {
+            if (!data[username].bet_match) {
+                const matchId = msg.split('_')[0];
+                const match = await DB.getMatchById(matchId);
+                data[username].bet_match = match;
+            }
+            
+            const matchId = data[username].bet_match.id;
+            const team1 = data[username].bet_match.team1;
+            const team2 = data[username].bet_match.team2;
+    
+            if (data[username].prediction_team2_goals != undefined) {
+                const won = msg;
+                let winner = '';  
+                if (won == team1) {
+                    winner = 'team1';
+                } else {
+                    winner = 'team2';
+                }
+                const team1Goals = data[username].prediction_team1_goals;
+                const team2Goals = data[username].prediction_team2_goals;
+                await DB.bet(matchId, team1Goals, team2Goals, winner, username);
+                data[username] = {};
+                bot.telegram.sendMessage(id, `Thank you, ${team1} ${team1Goals} - ${team2Goals} ${team2}, ${won} wins.`, mainMenu);
+            } else if (data[username].prediction_team1_goals != undefined) {
+                data[username].prediction_team2_goals = Number(msg);
+                if (data[username].prediction_team2_goals == data[username].prediction_team1_goals && !data[username].bet_match.is_group) {
+                    bot.telegram.sendMessage(id, `Which team wins? (/cancel)`, {
+                        "reply_markup": {
+                            keyboard: [
+                                [{text: team1}],
+                                [{text: team2}],
+                            ]
+                        }
+                    });
+                } else {
+                    let winner = null;
+                    const team1Goals = data[username].prediction_team1_goals;
+                    const team2Goals = data[username].prediction_team2_goals;
+                    if (team1Goals > team2Goals) {
+                        winner = 'team1';
+                    } else if (team1Goals < team2Goals) {
+                        winner = 'team2';
+                    }
+                    await DB.bet(matchId, team1Goals, team2Goals, winner, username);
+                    data[username] = {};
+                    bot.telegram.sendMessage(id, `Thank you, ${team1} ${team1Goals} - ${team2Goals} ${team2}`, mainMenu);
+                }
+    
+    
+    
+            } else if (data[username].prediction_getting_goals) {
+                data[username].prediction_team1_goals = Number(msg);
+                bot.telegram.sendMessage(id, `How many goals ${team2} will score? (/cancel)`, {});
+            } else {
+                data[username].prediction_getting_goals = true;
+                bot.telegram.sendMessage(id, `How many goals ${team1} will score? (/cancel)`, {});
+            }
+    
+        }
+    } catch(e) {
+        bot.telegram.sendMessage(id, `Error with code ${data[username].task}, Plz try again."`, mainMenu);
+    }
+
+    
+
+    
+});
+
+// bot.launch()
+
+// Enable graceful stop
+// process.once('SIGINT', () => bot.stop('SIGINT'))
+// process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+
+function validateUserName(username) {
+    return data.hasOwnProperty(username);
+}
+
+
+function sendStartAgainMessage(id) {
+    bot.telegram.sendMessage(id, 'Use /start again!', {reply_markup: { remove_keyboard: true }});
+}
+
+const mainMenu = {
+    "reply_markup": {
+        keyboard: [
+            [{text: "New Match"}],
+            [{text: "Match Started"}],
+            [{text: "Match Finished"}],
+            [{text: "Let's Bet!"}],
+            [{text: "Users Rank"}],
+        ]
+    }
+};
