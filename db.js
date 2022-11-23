c50f32fc9ece0dd9f4b25b10bd2c752f1cc6fb90
Parham Abr <abhar@globalworkandtravel.com>
2022-11-23 19:59:15 +0330

    Fix maybe

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ const GAME_STATUS = {
 }
 
 async function connect() {
-    client = new pg.Pool({
+    const pool = new pg.Pool({
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
         database: 'footballbetappdb',
@@ -20,12 +20,12 @@ async function connect() {
         idleTimeoutMillis: 30000,
         connectionTimeoutMillis: 2000,
     });
-    await client.connect();
+    client = await pool.connect();
 }
 
 
 async function calculateUserScores(matchId, team1Goals, team2Goals, winner, isGroup) {
-    await client.connect();
+    await pool.connect();
     const query = `SELECT * FROM user_match LEFT JOIN users
             ON users.id = user_match.user_id
             WHERE user_match.match_id = ${matchId}`;
@@ -67,7 +67,6 @@ async function calculateUserScores(matchId, team1Goals, team2Goals, winner, isGr
 
 export const DB = {
     newUser: async (username) => {
-        await client.connect();
         await connect();
         const users = await client.query(`
             select * from users where username = '${username}'
@@ -84,41 +83,41 @@ export const DB = {
     },
     
     newMatch: async (team1, team2, isGroup) => {
-        await client.connect();
+        await pool.connect();
         const query = `INSERT INTO matches(team1, team2, is_group, status) VALUES($1, $2, $3, $4)`;
         const values = [team1, team2, isGroup, GAME_STATUS.NOT_STARTED];
         await client.query(query, values);
     },
 
     getNotStartedMatches: async () => {
-        await client.connect();
+        await pool.connect();
         const query = `select * from matches where status = ${GAME_STATUS.NOT_STARTED}`;
         const result = await client.query(query);
         return result.rows;
     },
 
     getStartedMatches: async () => {
-        await client.connect();
+        await pool.connect();
         const query = `select * from matches where status = ${GAME_STATUS.STARTED}`;
         const result = await client.query(query);
         return result.rows;
     },
 
     setGameAsStarted: async(matchId) => {
-        await client.connect();
+        await pool.connect();
         const query = `UPDATE matches SET status = ${GAME_STATUS.STARTED} WHERE id = ${matchId}`;
         await client.query(query);
     },
 
     getMatchById: async(matchId) => {
-        await client.connect();
+        await pool.connect();
         const query = `SELECT * FROM matches WHERE id = ${matchId}`;
         const result = await client.query(query);
         return result.rows[0];
     },
 
     setGameAsFinished: async(matchId, team1Goals, team2Goals, winner, isGroup) => {
-        await client.connect();
+        await pool.connect();
         const query = `UPDATE matches SET status = ${GAME_STATUS.FINISHED},
                         team1_result = ${team1Goals},
                         team2_result = ${team2Goals},
@@ -130,7 +129,7 @@ export const DB = {
     },
 
     bet: async(matchId, team1Goals, team2Goals, winner, username) => {
-        await client.connect();
+        await pool.connect();
         let query = `SELECT * FROM users WHERE username = '${username}'`;
         let result = await client.query(query);
         const user = result.rows[0];
@@ -156,7 +155,7 @@ export const DB = {
     },
 
     getUsersRank: async() => {
-        await client.connect();
+        await pool.connect();
         let query = `SELECT * FROM users`;
         let result = await client.query(query);
         const users = result.rows;
@@ -164,7 +163,7 @@ export const DB = {
     },
 
     getUsersPrediction: async(matchId) => {
-        await client.connect();
+        await pool.connect();
         const fields = [
             'um.team1_goals',
             'um.team2_goals',
